73a1b46333e6452217a927695232b5d4275052b9
albertrick <albertrick1010@gmail.com>
2024-12-13 21:46:11 +0200

    1st commit menu infrastructure

---
+++ app/src/main/java/com/example/demo1801/Activities/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.demo1801.Activities;
+
+import android.os.Bundle;
+
+import com.example.demo1801.Objs.MasterActivity;
+import com.example.demo1801.R;
+
+public class MainActivity extends MasterActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/example/demo1801/Objs/MasterActivity.java
@@ -0,0 +1,27 @@
+package com.example.demo1801.Objs;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MasterActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        
+        return super.onOptionsItemSelected(item);
+    }
+}
